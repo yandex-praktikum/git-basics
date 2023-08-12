214015ff174c691c1ec7f2dffba2187fb8488417
Nikitin Pavel <seminarist@yandex.ru>
2023-08-12 13:12:05 +0300

    ООП

--- ООП.py
+++ ООП.py
@@ -1,5 +1,31 @@
 class FloatValue:
     def __set_name__(self, owner, name):
-        set.name = "_"+name
+        self.name = "_" + name
 
     def __set__(self, instance, value):
+        if type(value) != float:
+            raise TypeError("Присваивать можно только вещественный тип данных")
+        instance.__dict__[self.name] = value
+
+    def __get__(self, instance, owner):
+        return instance.__dict__[self.name]
+
+
+class Cell:
+    value = FloatValue()
+
+    def __init__(self, value=0.0):
+        self.value = value
+
+
+class TableSheet:
+    def __init__(self, N, M):
+        self.cells = [[Cell() for _ in range(M)] for _ in range(N)]
+
+
+table = TableSheet(5, 3)
+n = 1.0
+for i in range(5):
+    for j in range(3):
+        table.cells[i][j].value = n
+        n += 1
