d6539a391a3c44d7db06ad71ef289eabf9da1577
Nikitin Pavel <seminarist@yandex.ru>
2023-08-16 10:32:57 +0300

    ООП опять

--- ООП.py
+++ ООП.py
@@ -1,25 +1,42 @@
-class Thing:
-    def __init__(self, name, weight):
-        self.name = name
-        self.weight = weight
+class Circle:
+    attrs = {"x": (int, float), "y": (int, float), "radius": (int, float)}
 
-
-class Bag:
-    def __init__(self, max_weight):
-        self.max_weight = max_weight
-        self.__things = []
+    def __init__(self, x, y, radius):
+        self.__x = self.__y = self.__radius = None
+        self.x = x
+        self.y = y
+        self.radius = radius
 
     @property
-    def things(self):
-        return self.__things
+    def x(self):
+        return self.__x
 
-    def add_thing(self, thing):
-        s = self.get_total_weight()
-        if s + thing.weight <= self.max_weight
-            self.__things.append(thing)
+    @x.setter
+    def x(self, value):
+        self.__x = value
 
-    def remove_thing(self, indx):
-        self.__things.pop(indx)
+    @property
+    def y(self):
+        return self.__y
 
-    def get_total_weight(self):
-        return sum(t.weight for t in self.__things)
+    @y.setter
+    def y(self, value):
+        self.__y = value
+
+    @property
+    def radius(self):
+        return self.__radius
+
+    @radius.setter
+    def radius(self, value):
+        self.__radius = value
+
+    def __setattr__(self, key, value):
+        if key in self.attrs and type(value) not in self.attrs[key]:
+            raise TypeError('Неверный тип присваиваемых данных.')
+        if key == "radius" and value <= 0:
+            return
+        super().__setattr__(key, value)
+
+    def __getattr__(self, item):
+        return False
